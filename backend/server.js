bb83cf07235571f2f5359a87e537aed5348b9f66
samriddhitiwary <151003533+samriddhitiwary@users.noreply.github.com>
2025-02-08 15:56:46 +0530

    Base commit

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 7000;
+const URL = process.env.MONGODB_URL;
+
+mongoose
+  .connect(URL)
+  .then(() => {
+    console.log("DB connected successfully");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("DB connection failed:", error);
+  });
+
+
